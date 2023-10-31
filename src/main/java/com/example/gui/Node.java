dc7825cf226dfc47e90fb9d6747803db55af4dd5
jki12 <sjak0227@naver.com>
2023-10-31 11:20:46 +0900

    [fix] 노드 이동시 연결 준비 상태로 가는 버그 수정, 코드 수정 / recommit.

--- src/main/java/com/example/gui/Node.java
+++ src/main/java/com/example/gui/Node.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.UUID;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -14,15 +15,35 @@ public class Node extends JButton {
     private static final int WIDTH = 300;
     private static final int HEIGHT = 160;
 
+    private static Node selectedNode; // 현재 선택 되어 있는 노드.
+
+    private final UUID id;
+
     public Node(String type) {
         super(type);
+        Node cur = this;
+
+        id = UUID.randomUUID();
 
         setSize(WIDTH, HEIGHT);
 
-        this.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Connect plz");
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                
+                if (selectedNode == null) {
+                    selectedNode = cur;
+                    System.out.println("ready to connect");
+                }
+                else if (selectedNode == cur) { // 만약 같은 노드 클릭하면 reset 된다.
+                    System.out.println("");
+                    selectedNode = null;
+                }
+                else if (selectedNode != cur) { // TODO pairing, impl can pair func.
+                    String f = String.format("connect {%s} - {%s}", cur.id, selectedNode.id);
+
+                    System.out.println(f);
+                    selectedNode = null;
+                }
             }
         });
 
